565a200688b8a9248f7e3e9859716db44d674b61
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-05 15:08:43 +0100

    update the item and app controllers

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
 // Storage Controller
 
+
 // Item Controller
 const ItemCtrl = (function() {
-  
+  // Item contructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // Data structure / State
+  const data = {
+    items: [
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 50},
+      {id: 2, name: 'Fried eggs', calories: 300},
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
+
+  // Public methods
+  return {
+    logData: function() {
+      return data;
+    }
+  }
 })();
 
 
 // UI Controller
 const UICtrl = (function() {
   
+  // Public methods
+  return {
+
+  }
 })();
+
 
 // App Controller
   const App = (function(ItemCtrl, UICtrl) {
     
+    // Public methods
+    return {
+      init: function(){
+        console.log('init app');
+      }
+    }
+
   })(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init();
