d1e5fe97e240e06875d0c10fccdf74300a3d149e
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-05 15:04:20 +0100

    adds start for the controllers (Storage, Item, UI, App)

---
+++ app.js
@@ -0,0 +1,17 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+  
+})();
+
+
+// UI Controller
+const UICtrl = (function() {
+  
+})();
+
+// App Controller
+  const App = (function(ItemCtrl, UICtrl) {
+    
+  })(ItemCtrl, UICtrl);
