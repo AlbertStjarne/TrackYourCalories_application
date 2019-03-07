b8721eca6fa8eca7ebe5139ce9f78a95c70f06b2
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-07 14:50:20 +0100

    deleting item from list and adjusting total calories

--- app.js
+++ app.js
@@ -71,6 +71,18 @@ const ItemCtrl = (function() {
       });
       return found;
     },
+    deleteItem: function(id){
+      // Get ids
+      ids = data.items.map(function(item){
+        return item.id;
+      });
+
+      // Get index
+      const index = ids.indexOf(id);
+
+      // Remove item
+      data.items.splice(index, 1);
+    },
     setCurrentItem: function(item){
       data.currentItem = item;
     },
@@ -170,6 +182,11 @@ const UICtrl = (function() {
         }
       });
     },
+    deleteListItem: function(id){
+      const itemID = `#item-${id}`;
+      const item = document.querySelector(itemID);
+      item.remove();
+    },
     clearInput: function(){
       document.querySelector(UISelectors.itemNameInput).value = '';
       document.querySelector(UISelectors.itemCaloriesInput).value = '';
@@ -228,6 +245,12 @@ const UICtrl = (function() {
       
       // Update item event
       document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
+      // Delete item event
+      document.querySelector(UISelectors.deleteBtn).addEventListener('click', itemDeleteSubmit);
+
+      // Back button event
+      document.querySelector(UISelectors.backBtn).addEventListener('click', UICtrl.clearEditState);
     }
 
     // Add item submit
@@ -298,6 +321,27 @@ const UICtrl = (function() {
       
       UICtrl.clearEditState();
 
+      e.preventDefault();
+    }
+
+    // Delete button event
+    const itemDeleteSubmit = function(e) {
+      // Get current item
+      const currentItem = ItemCtrl.getCurrentItem();
+
+      // Delete from data structure
+      ItemCtrl.deleteItem(currentItem.id);
+
+      // Delete from UI
+      UICtrl.deleteListItem(currentItem.id);
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+      
+      UICtrl.clearEditState();
+      
       e.preventDefault();
     }
 
